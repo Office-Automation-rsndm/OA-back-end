fa970683c2da3e541d9a0d59018e086dc89f64e9
o-yyy <419311564@qq.com>
2021-12-06 00:45:26 +0800

    备注信息

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -9,6 +9,7 @@ public class UserService {
 
 
     public boolean login(String userName,String passwd) throws Exception{
+        int a = 1;
         return userdao.login(userName, passwd) == 1;//三目运算
     }
 
