231b38edb7c93695f1d3a93769cdf7cc7aad636d
o-yyy <419311564@qq.com>
2021-12-06 10:56:26 +0800

    添加mapper

---
+++ src/main/java/entity/Department.java
@@ -0,0 +1,49 @@
+package entity;
+
+public class Department {
+    private int id;
+    private int userNumbers;
+    private String name;
+
+    public Department(int id, int userNumbers, String name) {
+        this.id = id;
+        this.userNumbers = userNumbers;
+        this.name = name;
+    }
+
+    public Department() {
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", userNumbers=" + userNumbers +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserNumbers() {
+        return userNumbers;
+    }
+
+    public void setUserNumbers(int userNumbers) {
+        this.userNumbers = userNumbers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,75 +1,78 @@
 package entity;
 
 public class User {
-    /**
-     * user_id              int not null,
-     *    dept_id              int,
-     *    user_name          varchar(30),
-     *    user_creattime       datetime,
-     *    user_modifytime      datetime,
-     *    user_dept            varchar(30),
-     *    user_phone           varchar(11),
-     *    user_email           varchar(30),
-     *    user_pwd             varchar(12),
-     */
+    private int id;
+    private String name;
+    private String pwd;
+    private int noteId;
+    private int departmentId;
+    private String state;
 
-    private int user_id;  //用户id
-
-    private int dept_id;  //部门的id
-
-    private String user_name; //用户名
-
-    private String user_pwd; //用户密码
-
-    public int getUser_id() {
-        return user_id;
+    public User(String name, String pwd, int departmentId) {
+        this.name = name;
+        this.pwd = pwd;
+        this.departmentId = departmentId;
+        this.state = "正常";
     }
-
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-    }
-
-    public int getDept_id() {
-        return dept_id;
-    }
-
-    public void setDept_id(int dept_id) {
-        this.dept_id = dept_id;
-    }
-
-    public String getUser_name() {
-        return user_name;
-    }
-
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
-    }
-
-    public String getUser_pwd() {
-        return user_pwd;
-    }
-
-    public void setUser_pwd(String user_pwd) {
-        this.user_pwd = user_pwd;
-    }
-
-    public User() {
-    }
-
-    public User(int user_id, int dept_id, String user_name, String user_pwd) {
-        this.user_id = user_id;
-        this.dept_id = dept_id;
-        this.user_name = user_name;
-        this.user_pwd = user_pwd;
-    }
+    public User(){}
 
     @Override
     public String toString() {
         return "User{" +
-                "user_id=" + user_id +
-                ", dept_id=" + dept_id +
-                ", user_name='" + user_name + '\'' +
-                ", user_pwd='" + user_pwd + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", noteId=" + noteId +
+                ", departmentId=" + departmentId +
+                ", state='" + state + '\'' +
                 '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public int getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(int noteId) {
+        this.noteId = noteId;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
     }
 }
